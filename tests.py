99b29576416e4504ed217a745ecf48472a2d6cf5
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-07-30 14:38:51 +0300

    Test 1 done

--- tests.py
+++ tests.py
@@ -1,3 +1,24 @@
 import unittest
 from user_class import User
 from credentials_class import Credentials
+
+class TestPMAN(unittest.TestCase):
+
+    def tearDown(self):
+        User.userCredentials = []
+        Credentials.accountList = []
+
+    def setUp(self):
+        self.user = User ("Joe", "Boe", "JB", "12345")
+        self.acc = Credentials("guess", "guessyguess", "444")
+
+    def test_save_user(self):
+        '''
+        test_save_contact test case to test if the contact object is saved into
+        the contact list
+        '''
+        self.user.save_user() # saving the new contact
+        self.assertEqual(len(User.userCredentials),1)
+
+if __name__ == '__main__':
+    unittest.main()
