70467e16172225ba89ee93d1609f369d0cb6eebd
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-07-30 12:19:10 +0300

    Added new break point

--- pman.py
+++ pman.py
@@ -74,103 +74,110 @@ def main():
         user_save(new_user)
 
         print('--'*20)
-        print("Please use your details to now login")
-        print('\n')
-        print("Username: ")
-        userName = input()
-        print("Password: ")
-        passWord = input()
 
-        for usr in User.userCredentials:
-            if ((usr.username == userName) and (usr.password == passWord)):
+        while True:
+            print("Please use your details to now login")
+            print('\n')
+            print("Username: ")
+            userName = input()
+            print("Password: ")
+            passWord = input()
 
-                while True:
-                    print(f"Welcome {usr.first_name}! What would you like to do?")
-                    print("To add a new account type na, search for an account type sa, show all accounts type sc, delete an account type da and to exit type ex")
-                    code = input().lower()
+            for usr in User.userCredentials:
+                if ((usr.username == userName) and (usr.password == passWord)):
 
-                    if code == 'na':
-                        print("Let's add your account credentials to PMAN")
-                        print('--'*20)
-                        print('\n')
-                        print("Account Name: ")
-                        acc_name = input()
-                        for conts in Credentials.accountList:
-                            if conts.account_name == acc_name:
-                                print("Name already taken. Add another name!")
-                                print ("Account Name:...")
-                                acc_name = input()
+                    while True:
+                        print(f"Welcome {usr.first_name}! What would you like to do?")
+                        print("To add a new account type na, search for an account type sa, show all accounts type sc, delete an account type da and to exit type ex")
+                        code = input().lower()
 
-                        print("Account Username: ")
-                        acc_usrname = input()
-                        print("Account Password: ")
-                        print("Would you like us to create a password for you or would you like to use your own?")
-                        print("Type in 1 to have us generate a password and 2 for you to use yours")
-                        code_p = int(input())
-
-                        if code_p == 1:
-                            acc_pword = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
-                            new_Acc = Credentials(acc_name, acc_usrname, acc_pword)
-                            save_account(new_Acc)
-                            print(f"Your {acc_name} with password {acc_pword} has been created")
-                        elif code_p == 2:
-                            acc_pword = input()
-                            new_Acc = Credentials(acc_name, acc_usrname, acc_pword)
-                            save_account(new_Acc)
-                            print(f"Your {acc_name} with password {acc_pword} has been created")
-                        else:
-                            print("Please choose a number either 1 or 2 to proceed")
-
-                    elif code == 'sa':
-                        print(f"Thank you {u_name} for wanting to search. ")
-                        print("The search feature only works with account names.")
-                        print("Type in a valid account name and its details will be shown below:")
-                        searchTerm = input()
-                        if account_confirm(searchTerm):
-                            obtainedAcc = look_for_account(searchTerm)
-                            print("--"*20)
-                            print(f"The account {obtainedAcc.account_name} with password {obtainedAcc.account_password}")
-                        else:
-                            print("This account doesn't exist!")
-                    
-                    elif code =='sc':
-                        print("Your saved accounts are: ")
-                        print('--'*20)
-                        if len(Credentials.accountList) >= 1:
+                        if code == 'na':
+                            print("Let's add your account credentials to PMAN")
+                            print('--'*20)
+                            print('\n')
+                            print("Account Name: ")
+                            acc_name = input()
                             for conts in Credentials.accountList:
-                                print(f"Account Name: {conts.account_name} | Account Username: {conts.account_username} | Account Password: {conts.account_password}")
-                                print("--"*10)
-                                
-                        else:
-                            print("You have no accounts!")
+                                if conts.account_name == acc_name:
+                                    print("Name already taken. Add another name!")
+                                    print ("Account Name:...")
+                                    acc_name = input()
 
-                    elif code == 'da':
-                        print(f"Hello {u_name}. Below is a list of your saved accounts")
-                        print("Use the account name to search for the account you would like to delete")
-                        for const in Credentials.accountList:
-                            print(f"Account Name: {const.account_name}")
-                            print(f"Account Password: {const.account_password}")
-                            print("--"*10)
-                        searchAccount = input()
-                        if account_confirm(searchAccount):
-                            toBeDeleted = look_for_account(searchAccount)
-                            remove_account(toBeDeleted)
-                            print("Your new accounts list is: ")
-                            print("--"*10)
-                            for list in Credentials.accountList:
+                            print("Account Username: ")
+                            acc_usrname = input()
+                            print("Account Password: ")
+                            print("Would you like us to create a password for you or would you like to use your own?")
+                            print("Type in 1 to have us generate a password and 2 for you to use yours")
+                            code_p = int(input())
+
+                            if code_p == 1:
+                                acc_pword = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+                                new_Acc = Credentials(acc_name, acc_usrname, acc_pword)
+                                save_account(new_Acc)
+                                print(f"Your {acc_name} with password {acc_pword} has been created")
+                                print('\n')
+                            elif code_p == 2:
+                                acc_pword = input()
+                                new_Acc = Credentials(acc_name, acc_usrname, acc_pword)
+                                save_account(new_Acc)
+                                print(f"Your {acc_name} with password {acc_pword} has been created")
+                                print('\n')
+                            else:
+                                print("Please choose a number either 1 or 2 to proceed")
+
+                        elif code == 'sa':
+                            print(f"Thank you {u_name} for wanting to search. ")
+                            print("The search feature only works with account names.")
+                            print("Type in a valid account name and its details will be shown below:")
+                            searchTerm = input()
+                            if account_confirm(searchTerm):
+                                obtainedAcc = look_for_account(searchTerm)
+                                print("--"*20)
+                                print(f"The account {obtainedAcc.account_name} with password {obtainedAcc.account_password}")
+                                print('\n')
+                            else:
+                                print("This account doesn't exist!")
+                                print('\n')
+                        
+                        elif code =='sc':
+                            print("Your saved accounts are: ")
+                            print('--'*20)
+                            if len(Credentials.accountList) >= 1:
+                                for conts in Credentials.accountList:
+                                    print(f"Account Name: {conts.account_name} | Account Username: {conts.account_username} | Account Password: {conts.account_password}")
+                                    print("--"*10)
+
+                            else:
+                                print("You have no accounts!")
+                                print('\n')
+
+                        elif code == 'da':
+                            print(f"Hello {u_name}. Below is a list of your saved accounts")
+                            print("Use the account name to search for the account you would like to delete")
+                            for const in Credentials.accountList:
                                 print(f"Account Name: {const.account_name}")
                                 print(f"Account Password: {const.account_password}")
                                 print("--"*10)
+                            searchAccount = input()
+                            if account_confirm(searchAccount):
+                                toBeDeleted = look_for_account(searchAccount)
+                                remove_account(toBeDeleted)
+                                print("Your new accounts list is: ")
+                                print("--"*10)
+                                for list in Credentials.accountList:
+                                    print(f"Account Name: {const.account_name}")
+                                    print(f"Account Password: {const.account_password}")
+                                    print("--"*10)
 
-                    elif code == 'ex':
-                        print("Good bye! Thank you!")
+                        elif code == 'ex':
+                            print("Good bye! Thank you!")
 
-                    else:
-                        print("Please use a code to use the application!")
+                        else:
+                            print("Please use a code to use the application!")
 
-            else:
-                print("Please use your username and password")
-                break
+                else:
+                    print("Please use your username and password")
+                    break
 
 if __name__ == '__main__':
     main()
